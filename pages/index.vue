e8b0c033f645668070a91a9a53a542394fb799fb
osman mohammed <uniqueginun@gmail.com>
2021-12-31 23:59:04 +0300

    testing logout endpoint

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
     <h1>welcome to home screen</h1>
-    <h1>{{ $auth.user }}</h1>
+    <h1>Hi, {{ $auth.user.name }}</h1>
+    <h1>{{ $auth.user.email }}</h1>
+    <button @click="logout" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Logout</button>
   </div>
 </template>
 
 <script>
 export default {
-  middleware: 'auth'
+  middleware: 'auth',
+
+  methods: {
+    async logout() {
+      await this.$auth.logout();
+    }
+  }
 }
 </script>
