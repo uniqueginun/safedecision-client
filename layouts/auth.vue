01bab7aa69bfbfda02bd72718e7e29439b4b241a
osman mohammed <uniqueginun@gmail.com>
2022-01-01 01:58:37 +0300

    add auth layout

---
+++ layouts/auth.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex h-screen bg-gray-200 font-roboto">
+    <AuthSidebar />
+
+    <div class="flex-1 flex flex-col overflow-hidden">
+      <AuthHeaderVue />
+
+      <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+        <div class="container mx-auto px-6 py-8">
+          <Nuxt />
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import AuthHeaderVue from "~/components/layout/AuthHeader.vue"
+import AuthSidebar from "~/components/layout/AuthSidebar.vue"
+
+export default {
+   components: {
+      AuthHeaderVue,
+      AuthSidebar
+   }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <h1>welcome to home screen</h1>
-    <h1>Hi, {{ $auth.user.name }}</h1>
-    <h1>{{ $auth.user.email }}</h1>
-    <button @click="logout" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Logout</button>
+    home
   </div>
 </template>
 
@@ -11,10 +8,6 @@
 export default {
   middleware: 'auth',
 
-  methods: {
-    async logout() {
-      await this.$auth.logout();
-    }
-  }
+  layout: 'auth',
 }
 </script>
