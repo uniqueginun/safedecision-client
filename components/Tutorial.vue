fa8dc4c72683126a459ad4712e0fb59bc56e6c17
osman mohammed <uniqueginun@gmail.com>
2021-12-31 21:37:26 +0300

    install and configure nuxt auth module

--- components/Tutorial.vue
+++
@@ -1,7 +0,0 @@
-<!-- Please remove this file from your project -->
-<template>
-  <div>
-    <h1>Tutorial</h1>
-    <p>This is a tutorial component.</p>
-  </div>
-</template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,17 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/auth-next'
   ],
+
+  auth: {
+    strategies: {
+      laravelSanctum: {
+        provider: 'laravel/sanctum',
+        url: 'http://localhost:8000'
+      },
+    }
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
