f28daf7da5dc44c5912804f35548e7670849a36b
osman mohammed <uniqueginun@gmail.com>
2022-01-01 08:26:09 +0300

    set dynamic page title for admin pages

--- pages/_admin.vue
+++ pages/_admin.vue
@@ -1,5 +1,13 @@
 <template>
-   <nuxt-child />
+   <div class="mt-4">
+      <h4 class="text-gray-600">{{ pageTitle }}</h4>
+
+      <div class="mt-6">
+         <div class="my-6 overflow-hidden bg-white rounded-md shadow">
+            <nuxt-child />
+         </div>
+      </div>
+   </div>
 </template>
 
 <script>
@@ -7,5 +15,12 @@ export default {
    middleware: ['auth', 'admin'],
 
    layout: 'auth',
+
+   computed: {
+      pageTitle() {
+         const routeName = this.$route.name;
+         return routeName.replace('-', ' / ');
+      }
+   },
 }
 </script>
